e747eb56f1b822718a7cdfac29a48a88b61fa74a
Iván Morales <imorales@stratio.com>
2018-11-16 12:21:56 +0100

    Update format argument to plugin in ATests

--- src/test/java/com/stratio/qa/specs/ThenGIT.java
+++ src/test/java/com/stratio/qa/specs/ThenGIT.java
@@ -22,7 +22,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Factory;
 import com.stratio.qa.data.BrowsersDataProvider;
 
-@CucumberOptions(format = "json:target/cucumber.json", features ={
+@CucumberOptions(plugin = "json:target/cucumber.json", features ={
         "src/test/resources/features/readWebElementTextToVariable.feature",
         "src/test/resources/features/assertCommandExistsOnTimeOutIT.feature",
         "src/test/resources/features/assertSeleniumNElementExistsIT.feature",
